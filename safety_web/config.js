77b61d15c873aed57446cbb060264d6d807beb8e
Cherish599 <1146360747@qq.com>
2021-01-20 16:38:11 +0800

    使用更改了Nginx与后端搭建的相关内容

--- safety_web/config.js
+++ safety_web/config.js
@@ -4,8 +4,12 @@ module.exports = {
   proxy: {
     '/api': {
       // target: 'http://39.98.173.131:7000',
-      target: 'http://39.98.173.131:7000',
-      pathRewrite: {'^/api' : '/wlhse/api/v3'},
+      //target: 'http://39.98.173.131:7000',
+      target: 'http://localhost:8888',// 这是服务器地址
+      //http://localhost:8888/wlhse_war
+      // 接口地址映射 如前端请求 http://localhost:8080/api 映射到后端为 http://localhost:8888/wlhse/api/v3
+
+      pathRewrite: {'^/api' : '/wlhse_war/api/v3'},
       changeOrigin: true
     }
   }
